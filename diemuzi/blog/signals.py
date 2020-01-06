a72bb31cb91c277dc9cc0c9ca22c32daad969ddf
Sammie S. Taunton <diemuzi@gmail.com>
2020-01-06 17:05:38 -0600

    Removed hardcoded domain and replaced with sites domain

--- diemuzi/blog/signals.py
+++ diemuzi/blog/signals.py
@@ -1,11 +1,12 @@
 from django.conf import settings
+from django.contrib.sites.models import Site
 
 from utils.twit import Twitter
 
 
 def create_twitter_blog(sender, instance, created, **kwargs):
     if created:
-        url = 'https://local.diemuzi.com:8443/blog/' + instance.slug
+        url = 'https://' + Site.objects.get_current().domain + '/blog/' + instance.slug
 
         if settings.TWITTER_ACTIVE:
             Twitter().post(message='A new blog has been published - ' + url)
--- diemuzi/cook/signals.py
+++ diemuzi/cook/signals.py
@@ -1,11 +1,12 @@
 from django.conf import settings
+from django.contrib.sites.models import Site
 
 from utils.twit import Twitter
 
 
 def create_twitter_cook(sender, instance, created, **kwargs):
     if created:
-        url = 'https://local.diemuzi.com:8443/cook/' + instance.slug
+        url = 'https://' + Site.objects.get_current().domain + '/cook/' + instance.slug
 
         if settings.TWITTER_ACTIVE:
             Twitter().post(message='A new cooking recipe has been published - ' + url)
